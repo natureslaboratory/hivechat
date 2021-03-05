855c7a3d9f320f2fc66ff9bdd5b63d165c49c1ea
Jack Barber <jack@natureslaboratory.co.uk>
2021-03-05 19:04:25 +0000

    Getting local dev setup

--- perch/config/config.local.php
+++ perch/config/config.local.php
@@ -5,7 +5,7 @@
 	define('PERCH_SCHEDULE_SECRET', 'BXGkRfamUE6yDw73');
 
 	define('PERCH_DB_USERNAME', 'root');
-	define('PERCH_DB_PASSWORD', 'wbsCcvv234!');
+	define('PERCH_DB_PASSWORD', ',E?p5uKU8G]j-j)X');
 	define('PERCH_DB_SERVER', 	"localhost");
 	define('PERCH_DB_DATABASE', 'hivechat');
 	define('PERCH_DB_PREFIX', 	'perch3_');
