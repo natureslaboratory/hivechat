e3e0b86fb93b8a9cf78a056fc4b13311874cbb32
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-05-17 11:17:09 +0100

    added redirect to invites

--- perch/templates/pages/admin/invites.php
+++ perch/templates/pages/admin/invites.php
@@ -1,6 +1,13 @@
 <?php perch_layout('admin.header'); ?>
 <div class="app-main__outer">
     <div class="app-main__inner">
+    <?php
+        if (!perch_member_logged_in()) { ?>
+            <script>
+                window.location.href="/admin/login";
+            </script>
+        <?php }
+    ?>
 
     <?php get_invites(perch_member_get("email")) ?>
 
