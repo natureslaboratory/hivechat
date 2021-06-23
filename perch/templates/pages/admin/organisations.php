7ace226d7424ee8ffda1285904f295ef0d16ae79
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-06-23 09:46:35 +0100

    debug to check params

--- perch/templates/pages/admin/organisations.php
+++ perch/templates/pages/admin/organisations.php
@@ -2,7 +2,7 @@
 <div class="app-main__outer">
     <div class="app-main__inner">
         <?php 
-        
+            echo "manage: " . perch_get("manage") . ". hiveID: " . perch_get("hiveID") . "<br>";
         ?>
         <?php if (perch_get("hiveID")) { 
             $hiveID = perch_get("hiveID") | perch_get("manage");
