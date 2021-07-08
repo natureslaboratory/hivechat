33c1ff7902d282952f8b4efda4e61fcb4be3b3d0
Jack Barber <jack@natureslaboratory.co.uk>
2021-07-08 16:31:29 +0100

    Update home.php

--- perch/templates/pages/admin/home.php
+++ perch/templates/pages/admin/home.php
@@ -70,9 +70,9 @@ if(!perch_member_logged_in()){
 		const url = window.location.search;
 		const email = url.split("/").pop();
 		
-		function validateEmail(email) {
+		function validateEmail(email){
 			const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-			return re.test(String(email).toLowerCase();
+			return re.test(String(email).toLowerCase());
 		}
 		
 		if(validateEmail(email)){
